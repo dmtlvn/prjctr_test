19714e945a5717bc405b6199310ad254fe95a272
Dmitriy Levin <dmtlevin@gmail.com>
2022-08-14 13:53:45 +0200

    cleanup

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # prjctr_test
+
+To *"train"* a model, run:
+
+    ```python train.py --datadir DATADIR```
+
+The weights, predictions and metrics are saved to `logs/`.
+
+To start a server, run:
+
+    ```python app.py --model MODEL_FILE```
+
+The app is deployed to the Heroku, and can be accessed at https://prjctr-test.herokuapp.com/. 
+
+Example request:
+```
+curl -X POST https://prjctr-test.herokuapp.com/ \
+    -H 'Content-Type: application/json' \
+    -d '{"text":"test text"}'
+>>> {"score": -0.01125}
+```
---
+++ app.py
@@ -0,0 +1,35 @@
+import os
+import json
+import argparse
+from bottle import get, post, run, request, response
+from model import Model
+
+model = Model()
+
+
+@post('/')
+def do_evaluate():
+    try:
+        data = request.json
+    except Exception:
+        response.status = 400
+        return
+
+    score = model.predict_sample(data['text'])
+    response.headers['Content-Type'] = 'application/json'
+    return json.dumps({'score': score})
+
+
+@get('/')
+def get_help():
+    return "<a href=https://www.youtube.com/watch?v=JC0tqZfMv34>JC0tqZfMv34</a>"
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Run a web app')
+    parser.add_argument("--model", '-m', help="Path to the model weights")
+    args = parser.parse_args()
+
+    model.load(args.model)
+    port = os.environ.get("PORT", 8080)
+    run(host = '0.0.0.0', port = port, reloader = True)
